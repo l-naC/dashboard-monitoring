0921b88d1766a5aa28e1b71a3437c17cc5dbede0
Léna Clavier <lenaclavier@macbook-pro-de-lena.home>
2020-04-06 14:05:02 +0200

    new project with Firebase

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import firebase from 'firebase'
+
+Vue.config.productionTip = false;
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAsX6P5238gt7b_pP-IxMu7AmC0cXoIcrw",
+  authDomain: "dashboard-monitoring-58b33.firebaseapp.com",
+  databaseURL: "https://dashboard-monitoring-58b33.firebaseio.com",
+  projectId: "dashboard-monitoring-58b33",
+  storageBucket: "dashboard-monitoring-58b33.appspot.com",
+  messagingSenderId: "945931700919",
+  appId: "1:945931700919:web:a7e12949af55558a909b32",
+  measurementId: "G-LEGDP1GLT7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/SignUp.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="sign-up">
+    <h3>Sign Up</h3>
+    <p>Let's create à new account !</p>
+    <input type="text" v-model="email" placeholder="Email"><br>
+    <input type="password" v-model="password" placeholder="Password"><br>
+    <button @click="signUp">Sign Up</button><br>
+    <span>or go back to <router-link to="/login">login</router-link>.</span>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+
+export default {
+  name: 'signUp',
+  data() {
+      return {
+          email: '',
+          password: ''
+      };
+  },
+  methods: {
+      signUp: function() {
+        firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+        .then(function() {
+            alert('Your account has been created !')
+        }).catch(function(error) {
+            alert('Oops. ' + error.message)
+        });
+      }
+  }
+}
+</script>
+
+<style scoped>
+    .signUp{
+        margin-top: 40px;
+    }
+    input{
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button{
+        margin-top: 20px;
+        width: 10%;
+        cursor: pointer;
+    }
+    p{
+        margin-top: 40px;
+        font-size: 13px;
+    }
+    p a{
+        text-decoration: underline;
+        cursor: pointer;
+    }
+</style>
